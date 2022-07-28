c67cfe66214d59813d0380b14166023db60cd879
Uzair Chhapra <uzairchhapra@gmail.com>
2022-07-27 20:03:03 -0400

    script to download yt videos

---
+++ youtube.py
@@ -0,0 +1,69 @@
+from pytube import YouTube
+import getopt, sys
+from pytube.exceptions import RegexMatchError
+
+argumentList = sys.argv[1:]
+options = "ha:f:"
+long_options = ["help", "audio", "folder="]
+audioOnly = False
+urlProvided=''
+
+try:
+    arguments, values = getopt.getopt(argumentList, options, long_options)
+
+    for currentArgument, currentValue in arguments:
+
+        if currentArgument in ("-h", "--help"):
+            print("\nDownload Video:")
+            print('python youtube.py "url"\n')
+            print('Download Video to Folder:')
+            print('python youtube.py -f "path/to/folder" "url" \n')
+            print('Download Audio to Folder:')
+            print('python youtube.py -a -f "path/to/folder" "url"\n')
+            sys.exit()
+
+        elif currentArgument in ("-a", "--audio"):
+            audioOnly=True
+
+        elif currentArgument in ("-f", "--folder"):
+            path = currentValue
+
+    if len(values)!=1:
+        print('Enter URL of YouTube Video in quotes.\n')
+        print('Run the following command for help.')
+        print('python youtube.py -h')
+        sys.exit()
+
+    urlProvided=values[0]
+			
+except getopt.error as err:
+    print('Some Error occured!\nFollowing are examples on how to use the tool.\n')
+    print("Download Video:")
+    print('python youtube.py "url"\n')
+    print('Download Video to Folder:')
+    print('python youtube.py -f "path/to/folder" "url" \n')
+    print('Download Audio to Folder:')
+    print('python youtube.py -a -f "path/to/folder" "url"\n')
+    sys.exit()
+
+
+
+if urlProvided:
+    try:
+        yt = YouTube(urlProvided)
+
+        if audioOnly:
+            saved=yt.streams.get_audio_only().download(output_path=path)
+            print(f'Downloaded Audio and Saved here: {saved}')
+        else:
+            saved= yt.streams.get_highest_resolution().download(output_path=path)
+            print(f'Downloaded Video and Saved here: {saved}')
+
+    except RegexMatchError as r:
+        print(f'Incorrect YouTube URL provided: {urlProvided}')
+
+    except OSError as o:
+        print(f'{o.strerror}: {o.filename}')
+        
+    except Exception as e:
+        print('Unknown error occured.')
